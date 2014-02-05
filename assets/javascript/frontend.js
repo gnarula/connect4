477d8d8c7c2b2369eed0e802d289537e610df787
Gaurav Narula <gnarula94@gmail.com>
2014-02-05 23:30:11 +0530

    Notify Event

--- assets/javascript/frontend.js
+++ assets/javascript/frontend.js
@@ -6,4 +6,42 @@ $(document).ready(function() {
             $('#board td').last().addClass('box').attr('data-row', i).attr('data-column', j);
         }
     }
+
+    var socket = io.connect('localhost');
+
+    function Player(room, pid) {
+        this.room = room;
+        this.pid = pid;
+    }
+
+    var room = $('input').data('room');
+    var player = new Player(room, '', '');
+
+    socket.on('connect', function() {
+        socket.emit('join', {room: room});
+    });
+
+    socket.on('assign', function(data) {
+        player.color = data.color;
+        player.pid = data.pid;
+        if(player.pid == 1) {
+            $('.p1-score p').addClass('current');
+        }
+        else {
+            $('.p2-score p').addClass('current');
+        }
+    });
+
+    socket.on('leave', function() {
+        window.location = '/landingPage';
+    });
+
+    socket.on('notify', function(data) {
+        if(data.connected == 1) {
+            if(data.turn)
+                alertify.success('Players Connected! Your turn');
+            else
+                alertify.success('Players Connected! Opponent\'s turn');
+        }
+    });
 });
--- server.js
+++ server.js
@@ -6,11 +6,74 @@ var io = require('socket.io').listen(app.listen(8000));
 app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
-    res.render('index.jade');
+    share = generateRoom(6);
+    res.render('index.jade', {shareURL: req.protocol + '://' + req.get('host') + req.path + share, share: share});
 });
 
 app.get('/landingPage', function(req, res) {
     res.render('landing.jade');
+});
+
+app.get('/:room([A-Za-z0-9]{6})', function(req, res) {
+    share = req.params.room;
+    res.render('index.jade', {shareURL: req.protocol + '://' + req.get('host') + '/' + share, share: share});
+});
+
+
+function generateRoom(length) {
+    var haystack = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var room = '';
+
+    for(var i = 0; i < length; i++) {
+        room += haystack.charAt(Math.floor(Math.random() * 62));
+    }
+
+    return room;
+};
+
+// an object to hold all gamestates. Key denotes room id
+var games = {};
+
+io.sockets.on('connection', function(socket) {
+    socket.on('join', function(data) {
+        if(data.room in games) {
+            if(typeof games[data.room].player2 != "undefined") {
+                socket.emit('leave');
+                return;
+            }
+            socket.join(data.room);
+            socket.set('room', data.room);
+            socket.set('color', 'bar');
+            socket.set('pid', -1);
+            games[data.room].player2 = socket
+            // Set opponents
+            socket.set('opponent', games[data.room].player1);
+            games[data.room].player1.set('opponent', games[data.room].player2);
+
+            // Set turn
+            socket.set('turn', false);
+            socket.get('opponent', function(err, opponent) {
+                opponent.set('turn', true);
+            });
+
+            socket.emit('assign', {pid: 2});
+
+            games[data.room].player1.emit('notify', {connected: 1, turn: true});
+            socket.emit('notify', {connected: 1, turn: false});
+        }
+        else {
+            socket.join(data.room);
+            socket.set('room', data.room);
+            socket.set('color', 'foo');
+            socket.set('pid', 1);
+            socket.set('turn', false);
+            games[data.room] = {
+                player1: socket,
+                board: [[0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0]],
+            };
+            socket.emit('assign', {pid: 1});
+        }
+    });
 });
 
 console.log('Listening on port 8000');
